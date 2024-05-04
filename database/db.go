07223016c61f3d32ca86c69c1637b2f30a66f170
anderson <anderson.f.cardoso85@outlook.com>
2024-05-04 01:13:28 -0300

    Atualizacao db.go e dockerfile V4

--- database/db.go
+++ database/db.go
@@ -3,7 +3,6 @@ package database
 import (
 	"log"
 	"os"
-
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -22,5 +21,5 @@ func ConectaComBancoDeDados() {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+	DB.AutoMigrate(&models.Aluno{}
 }
